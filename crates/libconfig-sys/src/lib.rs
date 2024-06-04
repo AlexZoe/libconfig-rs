9ede0e5f054eaac79a7a445dfdd943d36746a74f
Alexander Zoellner <zoellner.contact@gmail.com>
2024-06-04 22:39:46 +0900

    [wip] Add Setting Lookup

--- crates/libconfig-sys/src/lib.rs
+++ crates/libconfig-sys/src/lib.rs
@@ -4,15 +4,19 @@ pub mod ffi {
     unsafe extern "C++" {
         include!("libconfig-sys/include/wrapper.h");
 
+        type Setting;
+
+        unsafe fn lookup(self: &Setting, path: *const c_char) -> Result<Pin<&mut Setting>>;
+
         type Config;
 
         #[cxx_name = "construct_unique"]
         fn Config_ctor() -> UniquePtr<Config>;
 
         unsafe fn readFile(self: Pin<&mut Config>, filename: *const c_char) -> Result<()>;
-        fn getOptions(self: &Config) -> i32;
-        fn setOptions(self: Pin<&mut Config>, options: i32);
-        fn clear(self: Pin<&mut Config>);
+
+        unsafe fn getRoot(self: &Config) -> Result<Pin<&mut Setting>>;
+        unsafe fn lookup(self: &Config, path: *const c_char) -> Result<Pin<&mut Setting>>;
 
         #[rust_name = "lookup_bool"]
         unsafe fn lookupValue(self: &Config, path: *const c_char, value: &mut bool) -> bool;
@@ -25,7 +29,11 @@ pub mod ffi {
         #[rust_name = "lookup_f64"]
         unsafe fn lookupValue(self: &Config, path: *const c_char, value: &mut f64) -> bool;
         #[rust_name = "lookup_string"]
-        unsafe fn lookupValue(self: &Config, path: *const c_char, value: Pin<&mut CxxString>) -> bool;
+        unsafe fn lookupValue(
+            self: &Config,
+            path: *const c_char,
+            value: Pin<&mut CxxString>,
+        ) -> bool;
 
         // Can't use "[unsigned] long long" directly for now
         unsafe fn lookupValueI64(config: &Config, path: *const c_char, value: &mut i64) -> bool;
